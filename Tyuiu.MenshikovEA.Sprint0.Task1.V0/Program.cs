7e6c976b6962c0863969585408c5fad4a9feabff
Tyuiu.MenshikovEA@outlook.com <menshikovegor045@gmail.com>
2023-09-05 15:31:49 +0500

    Task1

---
+++ Tyuiu.MenshikovEA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MenshikovEA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Egor";
+            int age = 18;
+            Console.WriteLine("Hello. My name is " + name + ", I am " + age + " years old!");
+            Console.ReadKey();
+        }
+    }
+}
