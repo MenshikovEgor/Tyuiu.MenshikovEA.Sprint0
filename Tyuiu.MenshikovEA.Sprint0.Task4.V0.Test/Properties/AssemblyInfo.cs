14d0d0ecf9f6456cea517966e88d480ed6d60a03
Tyuiu.MenshikovEA@outlook.com <menshikovegor045@gmail.com>
2023-09-12 00:51:47 +0500

    Task4

---
+++ Tyuiu.MenshikovEA.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MenshikovEA.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MenshikovEA.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c588462-7f1d-40be-b973-2d28a4ff6936")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MenshikovEA.Sprint0.Task4.V1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MenshikovEA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.MenshikovEA.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Addition
+            Console.WriteLine(DataService.Addition(1, 5));
+            //Substruction
+            Console.WriteLine(DataService.Substruction(15, 5));
+            //Multiplication
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            //Division
+            Console.WriteLine(DataService.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
