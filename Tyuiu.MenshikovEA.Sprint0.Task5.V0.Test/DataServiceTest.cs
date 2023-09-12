81f70999f390fbd89bac3a5f181203b02f7d8fdc
Tyuiu.MenshikovEA@outlook.com <menshikovegor045@gmail.com>
2023-09-12 16:25:33 +0500

    Task5

---
+++ Tyuiu.MenshikovEA.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MenshikovEA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.MenshikovEA.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        [TestMethod]
+        public void CheckedSubstrustionValid()
+        {
+            Assert.AreEqual(10, DataService.Substruction(15, 5));
+        }
+
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(10, DataService.Multiplication(2, 5));
+        }
+
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(10, DataService.Division(100, 10));
+        }
+    }
+}
---
+++ Tyuiu.MenshikovEA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MenshikovEA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.MenshikovEA.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(5, 5));
+            Console.WriteLine("A - B = " + DataService.Substruction(25, 5));
+            Console.WriteLine("A * B = " + DataService.Addition(5, 5));
+            Console.WriteLine("A / B = " + DataService.Division(25, 5));
+            Console.ReadKey();
+        }
+    }
+}
