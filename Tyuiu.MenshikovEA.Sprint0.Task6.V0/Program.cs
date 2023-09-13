6c57c1c7627434a772de22aaaba1182d0dd743c4
Tyuiu.MenshikovEA@outlook.com <menshikovegor045@gmail.com>
2023-09-13 19:54:45 +0500

    Task6

---
+++ Tyuiu.MenshikovEA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MenshikovEA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.MenshikovEA.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Sum of array items = " + DataService.AdditionArray(numsArray));
+
+            Console.WriteLine("Difference of array items = " + DataService.SubstractionArray(numsArray));
+
+            Console.WriteLine("Product of array items = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
